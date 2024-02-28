626aa8b0de2dec2ea8e1345bdb2b399518d8a687
Kaveesha <kaveeshasandaruwan1111@gmail.com>
2024-02-28 18:48:18 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const navListControl = document.getElementById('navListControl')
+
+// Nav List Dropdown
+navListControl.addEventListener('click', () => {
+    const navList = document.getElementById('navList')
+    const navListControl1 = document.getElementById('navListControl1')
+    const navListControl2 = document.getElementById('navListControl2')
+    const navListControl3 = document.getElementById('navListControl3')
+    
+    if (navList.style.right == '0px') {
+        navList.style.right = '-100vw'
+        navListControl1.style.transform = 'rotate(0) translateY(0)'
+        navListControl2.style.display = 'block'
+        navListControl3.style.transform = 'rotate(0) translateY(0)'
+    } 
+    else {
+        navList.style.right = '0px'
+        navListControl1.style.transform = 'rotate(45deg) translateY(4px)'
+        navListControl2.style.display = 'none'
+        navListControl3.style.transform = 'rotate(-45deg) translateY(-4px)'
+    }
+})
