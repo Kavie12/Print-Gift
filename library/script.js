95775b38d083715249ab5961feadaae5b99b1f42
Kaveesha <kaveeshasandaruwan1111@gmail.com>
2024-03-08 17:04:43 +0530

    blah

--- library/script.js
+++ library/script.js
@@ -339,3 +339,24 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 })
+
+
+
+
+
+
+
+// About FAQ Numbering
+
+function FAQNumbering() {
+    let faq = document.querySelectorAll('.faq-list > div > div > button')
+    for (let i = 0; i < faq.length; i++) {
+        faq[i].innerHTML = i + 1
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelectorAll('.faq-list')) {
+        FAQNumbering()
+    }
+})
