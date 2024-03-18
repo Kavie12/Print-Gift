54570e6f85cb212cddb5109266ae72665284d498
Kaveesha <kaveeshasandaruwan1111@gmail.com>
2024-03-18 08:36:28 +0530

    acc dropdown

--- library/global.js
+++ library/global.js
@@ -23,3 +23,60 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+
+
+
+
+// Login state Control
+
+document.addEventListener("DOMContentLoaded", () => {
+    let loginState = 1;
+
+    const loginBtn = document.querySelector(".navbar .login-btn");
+    const signBtn = document.querySelector(".navbar .sign-btn");
+    const navProfile = document.querySelector(".navbar .nav-profile");
+
+    if (loginState) {
+        loginBtn.style.display = "none";
+        signBtn.style.display = "none";
+        navProfile.style.display = "flex";
+    } else {
+        loginBtn.style.display = "flex";
+        signBtn.style.display = "flex";
+        navProfile.style.display = "none";
+    }
+});
+
+
+
+
+
+// Account Dropdown
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navProfile = document.querySelector(".navbar .nav-profile");
+    const menu = document.querySelector(".navbar .nav-profile-list");
+
+    navProfile.addEventListener("click", () => {
+        if (menu.style.display !== "flex") {
+            menu.style.display = "flex";
+        } else {
+            menu.style.display = "none";
+        }
+    });
+});
+
+
+
+
+
+
+// Nav Cart Orders Count
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cart = document.querySelector(".navbar .nav-cart > a > i > span");
+
+    cart.innerHTML = 2;
+});
